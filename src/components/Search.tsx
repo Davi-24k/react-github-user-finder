4fdec8524aaf238923a8e27c4a66e74513d796a1
Davi Manoel <davijm_00@outlook.com>
2024-02-07 08:57:05 -0300

    Exibição dos dados do User/Usuário

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,7 +2,7 @@ type SearchProps = {
     loadUser: (userName: string) => Promise<void>;
 };
 
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 
 import {BsSearch} from 'react-icons/bs';
 
@@ -10,6 +10,12 @@ import classes from "./Search.module.css";
 
 const Search = ({loadUser}: SearchProps) => {
     const [userName, setUserName] = useState("");
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if(e.key === "Enter") {
+            loadUser(userName);
+        }
+    };
 
     return (
         <div className={classes.search}>
@@ -19,6 +25,7 @@ const Search = ({loadUser}: SearchProps) => {
                 <input type="text" 
                 placeholder="Digite o nome do usuário" 
                 onChange={(e) => setUserName(e.target.value)}
+                onKeyDown={handleKeyDown}
                 />
                 <button onClick={() => loadUser(userName)}>
                     <BsSearch />
